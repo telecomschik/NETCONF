d1a21f61f968040a7dcbb484c0b0e61409f5d6b5
vartemenok <lancasters22@gmail.com>
2018-10-17 16:22:04 +0300

    Initial commit

---
+++ connector.py
@@ -0,0 +1,21 @@
+from devices import CSR1000V
+from ncclient import manager
+from pprint import pprint
+import xmltodict
+
+with manager.connect(**CSR1000V) as m:
+    c = m.get_config(source='running').data_xml
+    config_dict = xmltodict.parse(c)['data']
+    int_config = config_dict['interfaces'][1]['interface']
+    pprint(int_config)
+    for i in int_config:
+        try:
+            print('----------------------------------------------')
+            print('Interface: %s' % i['name'])
+            print('Description:%s' % i['description'])
+            print('IP:%s' % i['ipv4']['address']['ip'])
+            print('Mask:%s' % i['ipv4']['address']['netmask'])
+            print('----------------------------------------------')
+        except KeyError:
+            print('The key is not exist')
+
---
+++ devices.py
@@ -0,0 +1,7 @@
+CSR1000V = {
+    'host': 'ios-xe-mgmt.cisco.com',
+    'port': 10000,
+    'username': 'root',
+    'password': 'D_Vay!_10&',
+    'hostkey_verify': False
+}
